65c96773128791cc3a4aa7b813c09061cef79381
chamocell <venemexcoin@gmail.com>
2020-02-20 13:12:55 -0600

    implementacion de contador

---
+++ resources/views/contador.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="{{ asset('contador/img/logo.ico')}}" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{ asset('contador/css/estilos.css')}}">
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,600,700,900&display=swap" rel="stylesheet">
+    <script src="{{ asset('contador/js/simplyCountdown.min.js')}}"></script>
+    <title>VenemexCoin</title>
+</head>
+<body>
+
+<div class="portada" id="portada">
+        <div class="header">
+            <h1 class="logotipo">VenemexCoin</h1>
+            <p class="mensaje">sitio web en construcción</p>
+        </div>
+
+        <div id="cuenta"></div>
+
+        <div class="redes-sociales">
+            <a href="https://www.facebook.com/venemexcoin.venemexcoin.3" class="btn-red-social F1"><i
+                    class="fab fa-facebook-f"></i></a>
+            <a href="https://twitter.com/venemexcoin" class="btn-red-social T1"><i class="fab fa-twitter"></i></a>
+            <a href="https://vk.com/id540587465" class="btn-red-social Vk"><i class="fab fa-vk"></i></i></a>
+        </div>
+    </div>
+
+    <main class="contenedor">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus hic obcaecati fugit illo voluptatibus
+            reiciendis tempora sequi, harum enim eaque fugiat est! Fugiat ipsam eaque qui facilis esse sed voluptatem?
+            Quibusdam quasi voluptatum non fuga. Sit assumenda qui culpa nam vitae maiores, at enim minus eos soluta
+            numquam asperiores ratione iusto perspiciatis cumque? Tenetur dicta voluptates, doloremque ipsa itaque
+            cupiditate!
+            Suscipit placeat, officia enim labore, mollitia corporis molestiae quam amet voluptates quaerat ipsum ut
+            nostrum! Perferendis quidem sapiente, accusamus placeat voluptatem quae laboriosam animi earum nemo natus,
+            culpa eum quam.
+            Recusandae illo aliquam, animi fugit cupiditate blanditiis numquam! Laboriosam quos ullam optio distinctio
+            maiores dolorem pariatur quaerat nulla adipisci cum ratione provident, ducimus delectus quod illum dicta
+            nisi iusto mollitia?
+            A iste eius necessitatibus porro eaque architecto eveniet culpa voluptatibus non! Eius deleniti amet,
+            assumenda eveniet necessitatibus iste quasi facere laudantium dolores numquam commodi magnam ducimus
+            dignissimos quis! Alias, fugit.</p>
+    </main>
+
+
+
+ <script src="https://kit.fontawesome.com/3226d1ef96.js" crossorigin="anonymous"></script>
+<script src="{{ asset('contador/js/main.js')}}"></script>
+
+
+</body>
+</html>
